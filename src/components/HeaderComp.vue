b52a32ef9d1a375997b4b8f2868bf9411f55cfee
Paulo Renan Almeida <able.paulorenan@gmail.com>
2022-03-09 15:25:04 -0300

    cria componente header e reduz linhas no home]

---
+++ src/components/HeaderComp.vue
@@ -0,0 +1,66 @@
+<template>
+  <v-app-bar
+    app
+    color="white"
+  >
+    <v-container class="py-0 fill-height">
+      <img 
+        :src="imagem"
+        alt="Keep"
+        class="mx-auto rounded-circle"
+        style="width: 50px; height: 50px;"
+      />
+      <span
+        class="title"
+        style="font-size: 1.5rem; font-weight: bold; margin-left: 20px; margin-right: 10px;"
+      >{{ user.name }}</span>
+      <v-btn
+        text
+      >
+        Perfil
+      </v-btn>
+      <v-spacer></v-spacer>
+        <v-btn
+          icon
+          @click="logout"
+        >
+          Sair
+          <v-icon>mdi-logout</v-icon>
+        </v-btn>
+    </v-container>
+  </v-app-bar>
+</template>
+
+<script>
+import { mapState, mapActions } from 'vuex'
+
+  export default {
+    name: 'HeaderComp',
+    computed: {
+      ...mapState('auth', ['user']),
+      ...mapState('auth', ['tasks'])
+    },
+    data: () => {
+      return {
+        imagem: '',
+      }
+    },
+    created () {
+      this.verifyImagem()
+    },
+    methods: {
+      ...mapActions('auth', ['ActionSignOut']),
+      async logout() {
+        await this.ActionSignOut()
+        this.$router.push('/login')
+      },
+      verifyImagem() {
+        if (this.user.image === null) {
+          this.imagem = 'https://voxnews.com.br/wp-content/uploads/2017/04/unnamed.png'
+        } else {
+          this.imagem = this.user.imagem
+        }
+      },
+    },
+  }
+</script>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,53 +1,13 @@
 <template>
   <v-app id="inspire">
-    <v-app-bar
-      app
-      color="white"
-    >
-      <v-container class="py-0 fill-height">
-        <img 
-          :src="imagem"
-          alt="Keep"
-          class="mx-auto rounded-circle"
-          style="width: 50px; height: 50px;"
-        />
-        <span
-          class="title"
-          style="font-size: 1.5rem; font-weight: bold; margin-left: 20px; margin-right: 10px;"
-        >{{ user.name }}</span>
-        <v-btn
-          text
-        >
-          Perfil
-        </v-btn>
-        <v-spacer></v-spacer>
-          <v-btn
-            icon
-            @click="logout"
-          >
-            Sair
-            <v-icon>mdi-logout</v-icon>
-          </v-btn>
-      </v-container>
-    </v-app-bar>
+    <HeaderComp/>
     <v-main class="grey lighten-1" >
       <v-container class="mt-5">
-        <v-row
-          class="mx-auto"
-          justify="center"
-          align="center"
-        >
+        <v-row class="mx-auto" justify="center" align="center">
           <v-col>
-            <v-sheet
-              min-height="80vh"
-              rounded="lg"
-            >
+            <v-sheet min-height="80vh" rounded="lg" >
               <v-row class="mx-auto">
-                <v-col
-                  cols="12"
-                  md="8"
-                  offset-md="2"
-                >
+                <v-col cols="12" md="8" offset-md="2" >
                   <v-form ref="form">
                     <v-text-field
                       v-model="form.description"
@@ -73,28 +33,16 @@
                   v-for="task in tasks"
                   :key="task.id"
                 >
-                  <v-card
-                    outlined
-                    color="grey lighten-4"
-                  >
-                    <v-card-title>
-                      <span class="headline">{{ task.title }}</span>
-                    </v-card-title>
+                  <v-card outlined color="grey lighten-2" >
                     <v-card-text>
                       {{ task.description }}
                     </v-card-text>
                     <v-card-actions>
-                      <v-spacer></v-spacer>
-                      <v-btn
-                        color="primary"
-                        @click="editTask(task)"
-                      >
+                      <v-spacer/>
+                      <v-btn color="primary" @click="editTask(task)">
                         Editar
                       </v-btn>
-                      <v-btn
-                        color="error"
-                        @click="deleteTask(task)"
-                      >
+                      <v-btn color="error" @click="deleteTask(task)">
                         Excluir
                       </v-btn>
                     </v-card-actions>
@@ -111,17 +59,18 @@
 
 <script>
 import { mapState, mapActions } from 'vuex'
+import HeaderComp from '../components/HeaderComp.vue'
 
   export default {
     name: 'HomePage',
     computed: {
-      ...mapState('auth', ['user']),
       ...mapState('auth', ['tasks'])
+    },
+    components: {
+      HeaderComp
     },
     data: () => {
       return {
-        drawer: false,
-        imagem: '',
         loading: false,
         form: {
           description: '',
@@ -133,7 +82,6 @@ import { mapState, mapActions } from 'vuex'
     },
     created () {
       this.getTasks()
-      this.verifyImagem()
     },
     methods: {
       ...mapActions('auth', ['ActionGetTasks']),
@@ -141,13 +89,6 @@ import { mapState, mapActions } from 'vuex'
       ...mapActions('auth', ['ActionCreateTask']),
       async getTasks() {
         await this.ActionGetTasks()
-      },
-      verifyImagem() {
-        if (this.user.image === null) {
-          this.imagem = 'https://voxnews.com.br/wp-content/uploads/2017/04/unnamed.png'
-        } else {
-          this.imagem = this.user.imagem
-        }
       },
       async logout() {
         await this.ActionSignOut()
@@ -161,8 +102,6 @@ import { mapState, mapActions } from 'vuex'
         this.loading = false
         }
       },
-    },
-    components: {
     },
   }
 </script>
